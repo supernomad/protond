ba4199dc60ccdbbb4e32aa6df28f686a807aa3ee
Christian Saide Supernomad <csaide@quantumnet.ninja>
2017-02-27 00:54:12 -0500

    Added some docs to the input/filter modules

--- filter/doc.go
+++ filter/doc.go
@@ -1,4 +1,13 @@
 // Copyright (c) 2017 Christian Saide <Supernomad>
 // Licensed under the MPL-2.0, for details see https://github.com/Supernomad/protond/blob/master/LICENSE
 
+/*
+Package filter contains the interfaces, structs, and logic that form the basis of protonds filter plugin subsystem.
+
+Protond currently implements the following filter plugins:
+- Noop
+  - A no operation filter which just returns the event unchanged, this is used for pass through protond relays and testing protond.
+- Javascript
+  - This plugin allows for arbitrary javascript scripts that can modify and call certain functions on all events.
+*/
 package filter
--- input/doc.go
+++ input/doc.go
@@ -1,4 +1,13 @@
 // Copyright (c) 2017 Christian Saide <Supernomad>
 // Licensed under the MPL-2.0, for details see https://github.com/Supernomad/protond/blob/master/LICENSE
 
+/*
+Package input contains the interfaces, structs, and logic that form the basis of protonds input plugin subsystem.
+
+Protond currently implements the following input plugins:
+- Stdin
+  - An input plugin that reads from stdin and is used for testing filters and other pieces of functionality of protond.
+- TCP
+  - This plugin allows listening on an arbitrary tcp socket, and reads new line terminated strings from the connected clients.
+*/
 package input
